cb048fe23ed8cf0ee605a672f1a32ce3146426e6
patelavtx <apatel@aviatrix.com>
2023-04-11 09:32:36 +0100

    update to codeowners

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "avtx"
     workspaces {
-      name = "ace-iac-day-two"
+      name = "iac-day2"
     }
   }
 }
